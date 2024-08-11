ad6662cd6316b76d5351f9adc643d40e84625525
JhonSalgado22 <cuh51239696@cuh.mx>
2024-08-10 23:55:07 -0600

    Descripción de los cambios

--- src/main/java/tech/cybersys/videojuego_javafx/modelo/PersonajeDAO.java
+++
@@ -1,8 +0,0 @@
-package tech.cybersys.videojuego_javafx.modelo;
-
-import java.io.IOException;
-
-public interface PersonajeDAO {
-    void guardarPersonaje(Personaje personaje) throws IOException;
-    Personaje cargarPersonaje(String nombre) throws IOException;
-}
